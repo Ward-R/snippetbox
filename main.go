6506e65b9f4df3dc343e4c32be07566ad35bb3ca
Ward-R <4577045+Ward-R@users.noreply.github.com>
2025-07-22 11:36:56 +0900

    added basic routes

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+// This is the home handler function. Hello is the response body.
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from snippetbox"))
+}
+
+// snippetView handler
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
+	w.Write([]byte(msg))
+}
+
+// snippetCreate handler
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
 
 func main() {
-	fmt.Println("hello world")
+	// this starts a new mux(router). sets / pattern to home function
+	mux := http.NewServeMux()
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view/{id}", snippetView) // {id} wildcard
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Print("starting server on :4000")
+
+	// This starts a new server. Every HTTP request it gets it wills send to the mux
+	// to be routed. host:port
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
