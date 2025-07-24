752593c1a54afc22a2550af051e163a1fe7f60ee
Ward-R <4577045+Ward-R@users.noreply.github.com>
2025-07-24 12:10:48 +0900

    added error model. Added SQL view query

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"strconv"
 	"text/template"
+
+	"github.com/Ward-R/snippetbox/internal/models"
 )
 
 // This is the home handler function. Hello is the response body.
@@ -41,7 +44,21 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	// Use SnippetModel's Get() method to retrieve the data for a
+	// specific record based on its ID. If no matching record is found,
+	// Return a 404 Not Found Response.
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
+		return
+	}
+
+	// Write the snippet data as a plain-text HTTP response body.
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 // snippetCreate handler
@@ -51,9 +68,19 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 // snippetCreatePost handler
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	// Send a 201 created status code
-	w.WriteHeader(http.StatusCreated)
+	// Create seed data
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
+	expires := 7
 
-	// Write response body
-	w.Write([]byte("Save a new snippet..."))
+	// Pass the data to the SnippetModel.Insert() method, receiving the
+	// ID of the new record back
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	// Redirect the user to the relevant page for the snippet
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
