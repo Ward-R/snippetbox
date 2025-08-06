d31d12d947dea1996bcc401d48b3619648bb2515
Ward-R <4577045+Ward-R@users.noreply.github.com>
2025-08-06 14:29:40 +0900

    tests complete. Book done pre-guided exercises

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -18,6 +18,14 @@ func StringContains(t *testing.T, actual, expectedSubstring string) {
 	t.Helper()
 
 	if !strings.Contains(actual, expectedSubstring) {
-		t.Errorf("got: %q; expected to contain: @q", actual, expectedSubstring)
+		t.Errorf("got: %q; expected to contain: %q", actual, expectedSubstring)
+	}
+}
+
+func NilError(t *testing.T, actual error) {
+	t.Helper()
+
+	if actual != nil {
+		t.Errorf("got: %v; expected: nil", actual)
 	}
 }
